225672f63e7c733a6efe422f964d0cb449568176
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2020-12-05 14:31:59 +0100

    Day 5

--- 2020/05/Main.hs
+++ 2020/05/Main.hs
@@ -1,2 +1,24 @@
+import Data.Foldable
+import Data.List
+
 main :: IO ()
-main = error "unimplemented"
+main = do
+  let f (a, b) = (toInt a, toInt b)
+  let g (a, b) = (a, b, a * 8 + b)
+  xs <- sortOn (\(_, _, a) -> a) . fmap (g . f . splitAt 7 . fmap toBool) . lines <$> getContents
+  let ((_,_,x):_) = xs
+  let (_,_,y) = last xs
+  traverse_ print xs
+  traverse_ print $ [x..y] \\ fmap (\(_,_,a) -> a) xs
+
+toBool :: Char -> Bool
+toBool = \case
+  'F' -> False
+  'B' -> True
+  'L' -> False
+  _   -> True
+
+toInt :: [] Bool -> Int
+toInt = foldl' go 0
+  where
+  go acc n = acc * 2 + if n then 1 else 0
