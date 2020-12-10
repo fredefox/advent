dbb71da99aadf6d9d8ec9465a7b2b64598eb3200
Frederik Hanghøj Iversen <fhanghojiversen@zendesk.com>
2020-12-10 13:36:08 +0100

    Day 10
    Today my solution is not very fast:
    
        ./Main < input  2.29s user 0.02s system 99% cpu 2.316 total

--- 2020/10/Main.hs
+++ 2020/10/Main.hs
@@ -1,2 +1,34 @@
+{-# language ScopedTypeVariables #-}
+import Data.List
+import Data.Foldable
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 main :: IO ()
-main = error "unimplemented"
+main = do
+  xs' <- sort . fmap read . lines <$> getContents
+  let xs = xs' <> [last xs' + 3]
+  traverse_ print $ fmap length $ group $ sort $ solve xs
+  let xs'' = 0 : xs
+  print $ pathsC (minimum xs'') (maximum xs'') $ adjecency xs''
+
+solve :: [Int] -> [Int]
+solve xs = fmap fst $ tail $ scanl (\(_acc, a0) a1 -> (a1 - a0, a1)) (0, 0) $ sort xs
+
+adjecency :: Integral n => [n] -> Map n [n]
+adjecency xs = Map.fromListWith (<>) $ zipWith (\x xs' -> (x, takeWhile (<= x + 3) $ tail xs')) xs $ tails xs
+
+pathsC:: forall n . Integral n => Ord n => n -> n -> Map n [n] -> n
+pathsC s0 t m = go s0 mempty
+  where
+  go :: n -> Map n n -> n
+  go s visited
+    | s == t    = 1
+    | otherwise = case Map.lookup s visited of
+        Nothing -> case Map.lookup s m of
+          Nothing -> 0
+          Just xs -> snd $ foldr go' (visited, 0) xs
+            where
+            go' :: n -> (Map n n, n) -> (Map n n, n)
+            go' x (m', acc) = let k = go x m' in (Map.insert x k m', k + acc)
+        Just res -> res
