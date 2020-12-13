cf97048c6e531f9235b6eb5eb0f29320267b262b
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2020-12-13 15:49:20 +0100

    Pt2

--- 2020/12/Main.hs
+++ 2020/12/Main.hs
@@ -5,6 +5,7 @@ import Data.Foldable
 import Data.Monoid
 import Control.Monad.State
 import Data.Coerce
+import Debug.Trace
 
 main :: IO ()
 main = do
@@ -14,10 +15,10 @@ main = do
   putStrLn $ ppr $ run xs
 
 ppr :: S -> String
-ppr (S _ (V2 (Sum a) (Sum b))) = unwords [show a, show b]
+ppr (S (V2 (Sum a) (Sum b)) (V2 (Sum c) (Sum d))) = unwords [show a, show b, "|", show c, show d]
 
 run :: [(Char, Int)] -> S
-run xs = execState (traverse_ step xs) $ S (V2 1 0) mempty
+run xs = execState (traverse_ step xs) $ S (V2 0 0) (V2 10 (-1))
 
 parse :: String -> (Char, Int)
 parse [] = error "Parse error"
@@ -34,8 +35,8 @@ instance Monoid a => Monoid (V2 a) where
 deriving stock instance Show a => Show (V2 a)
 
 data S = S
-  { direction :: V2 (Sum Int)
-  , position  :: V2 (Sum Int)
+  { position  :: V2 (Sum Int)
+  , waypoint  :: V2 (Sum Int)
   }
 
 deriving stock instance Show S
@@ -51,26 +52,33 @@ scale n (V2 a0 a1) = V2 (a0 * n) (a1 * n)
 step :: (Char, Int) -> Ship ()
 step (c, n) = state f
   where
-  f S{ direction, position } = ((), S direction' position' )
+  f :: S -> ((), S)
+  f S{ position, waypoint } = ((), let x = S position' waypoint' in trace (show (c, n) <> " " <> ppr x) x )
     where
-    direction' :: V2 (Sum Int)
-    direction' = case c of
-      'R' -> rotate direction $ rotation quarterTurns
-      'L' -> rotate direction $ rotation $ negate quarterTurns
-      _   -> direction
+    -- direction' :: V2 (Sum Int)
+    -- direction' = case c of
+    --   'R' -> rotate direction $ rotation quarterTurns
+    --   'L' -> rotate direction $ rotation $ negate quarterTurns
+    --   _   -> direction
     quarterTurns = n `div` 90
     rotation m = case m `mod` 4 of
       0 -> V2 1 0
       1 -> V2 0 1
       2 -> V2 (-1) 0
-      3 -> V2 0 (-1)
+      _ -> V2 0 (-1)
     position' :: V2 (Sum Int)
     position' = position <> delta
     delta :: V2 (Sum Int)
     delta = coerce $ case c of
+      'F' -> scale n $ coerce waypoint
+      _   -> V2 0 0
+    waypoint' :: V2 (Sum Int)
+    waypoint' = rotate (waypoint <> deltaWaypoint) $ rotation $ case c of
+      'R' -> quarterTurns
+      _ -> negate quarterTurns
+    deltaWaypoint = coerce $ case c of
       'N' -> V2 0 (negate n)
       'S' -> V2 0 n
       'W' -> V2 (negate n) 0
       'E' -> V2 n 0
-      'F' -> scale n $ coerce direction
       _   -> V2 0 0
