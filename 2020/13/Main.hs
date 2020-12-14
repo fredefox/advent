190225e8ff1ee5976fb8c23c80d4f4f4a1902115
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2020-12-14 13:38:22 +0100

    Day 14

--- 2020/13/Main.hs
+++ 2020/13/Main.hs
@@ -1,16 +1,25 @@
 {-# language OverloadedStrings #-}
 {-# language TypeApplications #-}
+{-# language StandaloneDeriving #-}
+{-# language DerivingStrategies #-}
 import Text.Read
 import Control.Applicative
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Data.List
 import Data.Foldable
+import Data.Maybe
 
 main :: IO ()
 main = do
-  (n, xs) <- getInput
-  traverse_ print $ part1 n xs
+  (_, xs) <- getInput
+  -- traverse_ print $ part1 n xs
+  -- print xs
+  -- print $ catMaybes $ zipWith (\m n -> fmap (\m -> mkN m n) m) xs [0..]
+  let ys = catMaybes $ zipWith (\m n -> fmap (\m -> mkN m n) m) xs [0..]
+  traverse_ print $ runN $ mconcat ys
+  -- traverse_ print $ runN $ mkN 59 4 <> mkN 7 0 <> mkN 13 1 <> mempty
+  -- traverse_ print $ runN $ mconcat [mkN 19 7, mkN 31 6, mkN 59 4, mkN 7 0, mkN 13 1]
 
 part1 :: Int -> [Int] -> [(Int, Int)]
 part1 n xs = sortOn (negate . snd) $ go <$> xs
@@ -20,15 +29,45 @@ part1 n xs = sortOn (negate . snd) $ go <$> xs
       where
       (a, b) = divMod n m
 
+data N = N { runN :: [Int] }
 
-getInput :: IO (Int, [] Int)
+mkN :: Int -> Int -> N
+mkN n m = N $ (+ m) <$> iterate (+n) 0
+
+deriving stock instance Show N
+
+instance Semigroup N where
+  N xs <> N ys = N $ merge xs ys
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs@(x:xss) ys@(y:yss) = case x `compare` y of
+  LT -> merge xss ys
+  EQ -> x : merge xss yss
+  GT -> merge xs yss
+merge _ _ = mempty
+
+instance Monoid N where
+  mempty = mkN 1 0
+
+getInput :: IO (Int, [Maybe Int])
 getInput = do
   lns <- fmap Text.lines Text.getContents
   case lns of
-    (n : xs : _) -> pure (read @Int $ Text.unpack n, foldMap (parseNum @[] . Text.unpack) $ Text.splitOn "," xs)
+    (n : xs : _) -> pure (read @Int $ Text.unpack n, fmap (parseNum @Maybe . Text.unpack) $ Text.splitOn "," xs)
     _ -> fail "Parse error"
 
 parseNum :: Alternative m => String -> m Int
 parseNum s = case readMaybe s of
   Nothing -> empty
   Just x -> pure x
+
+-- lcm 3 4 = 12
+-- 3 0 -> 0   3   6 9 12
+-- 4 1 ->   1   5   9    13
+-- f (3 0) (4 1) = 9 < 12 = lcm 3 4
+
+-- k0 * a0 + a1 = k1 * b0 + b1
+-- k0 * a0 + (a1 - b1) = k1 * b0
+-- (k0 * a0 + (a1 - b1) `div` b0) = k1
+meet :: (Int, Int) -> (Int, Int) -> (Int, Int)
+meet (a0, a1) (b0, b1) = (k0 * a0 + (a1 - b1)) `div` b0
--- 2020/14/Main.hs
+++ 2020/14/Main.hs
@@ -1,2 +1,49 @@
+{-# language OverloadedStrings #-}
+{-# language ScopedTypeVariables #-}
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import Data.Foldable
+import Data.Bits
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 main :: IO ()
-main = error "unimplemented"
+main = do
+  xs <- getInput
+  let l = Map.toList $ solve1 xs
+  traverse_ print l
+  print $ sum $ snd <$> l
+
+solve1 :: [(Int, Int, [(Int, Int)])] -> Map Int Int
+solve1 xs0 = Map.fromList $ foldMap go xs0
+  where
+  go :: (Int, Int, [(Int, Int)]) -> [(Int, Int)]
+  go (m, v, xs) = fmap (fmap $ mask m v) xs
+
+getInput :: forall a . Bits a => Read a => IO [(a, a, [(a, a)])]
+getInput = fmap (f . uncons . Text.lines) . Text.splitOn "\n\n" <$> Text.getContents
+  where
+  g :: Text -> (a, a)
+  g ln = case Text.words ln of (a : b : _) -> (read $ Text.unpack a, read $ Text.unpack b) ; _ -> undefined
+  f :: (Text, [Text]) -> (a, a, [(a, a)])
+  f (x, xs) = (m, v, g <$> xs)
+    where
+    go c n = case c of
+          'X' -> bit n
+          _   -> zeroBits
+    go' c n = case c of
+          '1' -> bit n
+          _   -> zeroBits
+    m = buildWith go x
+    v = buildWith go' x
+
+buildWith :: Bits a => (Char -> Int -> a) -> Text -> a
+buildWith f x = foldl' (.|.) zeroBits $ zipWith f (Text.unpack $ Text.reverse x) [0..]
+
+uncons :: [a] -> (a, [a])
+uncons (x:xs) = (x, xs)
+uncons _ = error "Empty list"
+
+mask :: Bits a => a -> a -> a -> a
+mask m v x = (complement m .&. v) .|. (m .&. x)
