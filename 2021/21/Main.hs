9606a542fa2045bcd43b3848071cf53aa5b7bdd9
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2021-12-23 14:16:54 +0100

    Day 21 pt 2
    Completes in 100 ms on my machine with -O2

--- 2021/21/Main.hs
+++ 2021/21/Main.hs
@@ -1,55 +1,53 @@
-{-# language LambdaCase #-}
 {-# language TypeApplications #-}
-import Control.Monad.State
 import Data.Foldable
 import System.Environment
+import Data.Array (Array, (!))
+import qualified Data.Array as Array
+import qualified Data.Ix as Ix
 
 main :: IO ()
 main = do
-  (a:b:_) <- fmap (read @Int) <$> getArgs
-  let xs = evalState steps (join $ repeat [1..100], 0, a, b)
-  traverse_ print $ scanl plus (0, 0, 0) xs
+  (p0:p1:n:_) <- fmap (read @Int) <$> getArgs
+  let three = [1,2,3]
+      rolls :: [Int]
+      rolls = do
+        a <- three
+        b <- three
+        c <- three
+        pure $ sum [a, b, c]
+  print $ uncurry max $ solveA rolls n (p0, 0, p1, 0)
 
-type I = (Int, Int, Int)
+-- | Player positions and scores.
+type K = (Int, Int, Int, Int)
 
-plus :: I -> I -> I
-plus (_, a0, a1) (n, b0, b1) = (n, a0 + b0, a1 + b1)
+-- | Maps player positions, current positions and scores to number of
+-- wins.
+type A = Array K U
 
-type S a = State ([Int], Int, Int, Int) a
+solveA :: [Int] -> Int -> K -> U
+solveA rolls n k = arr ! k
+  where
+  arr :: A
+  arr = Array.listArray bounds $ f <$> Ix.range bounds
+  bounds = ((1, 0, 1, 0), (10, n, 10, n))
+  f :: K -> U
+  f (p0, s0, p1, s1) = foldl' plus (0, 0) $ g <$> rolls
+    where
+    g :: Int -> U
+    g roll
+      | s0' >= n  = (1, 0)
+      | otherwise = swap $ arr ! (p1, s1, p0', s0')
+        where
+        p0' = mod10 $ roll + p0
+        s0' = p0' + s0
 
-roll :: S Int
-roll =
-  get >>= \case
-    (x:xs, n, p1, p2) -> do
-      put (xs, succ n, p1, p2)
-      pure x
-    _ -> undefined
+type U = (Int, Int)
 
-roll3 :: S Int
-roll3 = do
-  a <- roll
-  b <- roll
-  c <- roll
-  pure $ sum [a,b,c]
+plus :: U -> U -> U
+plus (a0, a1) (b0, b1) = (a0 + b0, a1 + b1)
 
-step :: S I
-step = do
-  q1 <- roll3
-  q2 <- roll3
-  (xs, n, p1, p2) <- get
-  let r1 = mod10 $ p1 + q1
-  let r2 = mod10 $ p2 + q2
-  put (xs, n, r1, r2)
-  pure (n, r1, r2)
-  
-steps :: S [I]
-steps = repeatM step
+swap :: U -> U
+swap (a, b) = (b, a)
 
-repeatM :: Applicative m => m a -> m [a]
-repeatM m = (:) <$> m <*> repeatM m
-
--- |
--- >>> mod10 <$> [-1,0,1,10]
--- [9,10,1,10]
 mod10 :: Int -> Int
 mod10 n = succ $ pred n `mod` 10
