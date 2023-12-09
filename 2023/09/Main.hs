4b0c549fdf9b0b92b64b9a0505633eb74ab7284d
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2023-12-09 11:42:27 +0100

    simplify

--- 2023/09/Main.hs
+++ 2023/09/Main.hs
@@ -1,24 +1,21 @@
-{-# language LambdaCase #-}
 {-# options_ghc -Wall #-}
 module Main (main) where
 
 main :: IO ()
 main = do
-  xs <- fmap (fmap (read @Int) . words) . lines <$> getContents
+  xs <- parse <$> getContents
   print $ sum $ next <$> xs
-  print $ sum $ previous <$> xs
+  print $ sum $ next . reverse <$> xs
 
-previous :: Integral n => [n] -> n
-previous x = foldl (flip (-)) 0 $ reverse $ head <$> (diffs $ x)
+parse :: String -> [[Int]]
+parse = fmap (fmap (read @Int) . words) . lines
 
 next :: Integral n => [n] -> n
 next x = sum $ last <$> diffs x
 
 diffs :: Integral n => [n] -> [[n]]
-diffs = f . span (any (/= 0)) . go
+diffs
+  = takeWhile (any (/= 0))
+  . iterate diff
   where
-  go = \case
-    xs@(_:xss) -> xs : (go $ zipWith (-) xss xs)
-    [] -> []
-  f (xs, []) = xs
-  f (xs, y:_) = xs <> [y]
+  diff xs = zipWith subtract xs (tail xs)
