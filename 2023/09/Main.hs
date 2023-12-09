d271dd66a1ae0a2930fe374ea9661d366a306aef
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2023-12-09 11:29:48 +0100

    Part 2

--- 2023/09/Main.hs
+++ 2023/09/Main.hs
@@ -5,10 +5,14 @@ module Main (main) where
 main :: IO ()
 main = do
   xs <- fmap (fmap (read @Int) . words) . lines <$> getContents
-  print $ sum $ one <$> xs
+  print $ sum $ next <$> xs
+  print $ sum $ previous <$> xs
 
-one :: Integral n => [n] -> n
-one x = sum $ last <$> diffs x
+previous :: Integral n => [n] -> n
+previous x = foldl (flip (-)) 0 $ reverse $ head <$> (diffs $ x)
+
+next :: Integral n => [n] -> n
+next x = sum $ last <$> diffs x
 
 diffs :: Integral n => [n] -> [[n]]
 diffs = f . span (any (/= 0)) . go
