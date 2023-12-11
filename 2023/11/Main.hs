8f71529add30c676fcce09468eac9fd9539d506f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2023-12-11 16:07:09 +0100

    Part 2

--- 2023/11/Main.hs
+++ 2023/11/Main.hs
@@ -1,18 +1,34 @@
 {-# language Haskell2010, LambdaCase #-}
+{-# options_ghc -Wall #-}
 module Main (main) where
 
 import qualified Data.List as List
 import Data.Array (Array)
 import qualified Data.Array as Array
 import Control.Monad
-import Data.Coerce
 
 main :: IO ()
 main = do
   xs <- lines <$> getContents
-  let m = expand xs
-  let gs = fmap fst $ filter (\(_, c) -> c /= '.') $ Array.assocs $ matrix m
-  print $ sum $ uncurry dist <$> pairs gs
+  let gs = fmap fst $ filter (\(_, c) -> c /= '.') $ Array.assocs $ matrix xs
+  print $ solve 1 xs gs
+  print $ solve (pred 1000000) xs gs
+
+solve :: Int -> [String] -> [(Int, Int)] -> Int
+solve k xs gs = sum $ uncurry dist <$> pairs gs'
+  where
+  row = deltas k xs
+  col = deltas k $ List.transpose xs
+  gs' = (\(i, j) -> (row i, col j)) <$> gs
+
+deltas :: Int -> [String] -> Int -> Int
+deltas k xs i = i + a Array.! i
+  where
+  go n s = if all (== '.') s then n + k else n
+  a = listArray $ scanl go 0 xs
+
+listArray :: [a] -> Array Int a
+listArray xs = Array.listArray (0, pred $ length xs) xs
 
 pairs :: [a] -> [(a, a)]
 pairs = \case
@@ -21,13 +37,6 @@ pairs = \case
 
 dist :: Num a => (a, a) -> (a, a) -> a
 (a0, b0) `dist` (a1, b1) = abs (a0 - a1) + abs (b0 - b1)
-
-expand :: [String] -> [String]
-expand = List.transpose . foldMap go . List.transpose . foldMap go
-  where
-  go :: String -> [String]
-  go xs | all (== '.') xs = [xs, xs]
-        | otherwise = [xs]
 
 matrix :: [[a]] -> Array (Int, Int) a
 matrix = \case
