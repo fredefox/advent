25d7df2efaa697a9e9b7e042c65553ee4db7a48b
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2023-12-15 23:06:47 +0100

    2023-12-13 pt 2

--- 2023/13/Main.hs
+++ 2023/13/Main.hs
@@ -9,15 +9,41 @@ import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Control.Applicative
 
+fromList :: a -> [a] -> a
+fromList d = \case
+  [] -> d
+  (x:_) -> x
+
 main :: IO ()
 main = do
   xs <- fmap Text.unpack . Text.splitOn "\n\n" <$> Text.getContents
-  print $ sum $ solve <$> xs
+  print $ sum $ (fromList undefined . solve) <$> xs
+  print $ sum $ (fromList undefined . solve2) <$> xs
 
-solve :: String -> Int
-solve p = case go ls <|> (*100) <$> (go rs) of
-  [] -> error "No reflection found"
-  (x:_) -> x
+-- | Find all new reflection lines arising from a smudge anywhere.
+solve2 :: String -> [Int]
+solve2 x = foldMap go $ replacements x
+  where
+  go r = solve r List.\\ solve x
+
+replacements :: String -> [String]
+replacements = \case
+  [] -> [[]]
+  (x:xs) -> a <|> b
+    where
+    a = do
+      x' <- smudge x
+      pure $ x':xs
+    b = (x:) <$> replacements xs
+
+smudge :: Char -> String
+smudge = \case
+  '.' -> pure '#'
+  '#' -> pure '.'
+  _ -> empty
+
+solve :: String -> [Int]
+solve p = go ls <|> (*100) <$> (go rs)
   where
   ls = lines p
   rs = List.transpose ls
