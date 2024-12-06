e02c2e6829025a74632e433e594791f949a35615
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-12-07 00:45:56 +0100

    refactor

--- 2024/06/Main.hs
+++ 2024/06/Main.hs
@@ -1,12 +1,5 @@
--- I haven't done part 2, but my idea for solving it is writing an
--- algorithm to determine if the guard is in a loop with the current
--- configuration and then for each position where we can put an
--- additional obstacle, check if that configuration means there is a
--- loop and then count them all.  I envisage that this solution will
--- have time complexity O(n ^ 2) where n is the size of the maze
--- (number of cells).
-
 {-# language GHC2021, LambdaCase, DerivingStrategies #-}
+{-# options_ghc -Wall #-}
 module Main (main) where
 
 import Data.Array (Array, Ix)
@@ -14,8 +7,6 @@ import qualified Data.Array as Array
 import Control.Monad
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.Foldable
-import System.IO
 
 main :: IO ()
 main = do
@@ -24,14 +15,16 @@ main = do
   indicesOf ('^' ==) m `forM_` \idx -> do
     let w = walk heading m idx
     print $ areaOfWalk w
-    let ns = scanl @Int (go heading m idx) 0 (Array.indices m)
-    print $ last ns
+    print $ solve heading m idx (Array.indices m)
 
-go :: Heading (Int, Int) -> Array (Int, Int) Char -> (Int, Int) -> Int -> (Int, Int) -> Int
-go heading m idx0 n idx = if repitition w' then succ n else n
+solve :: Heading (Int, Int) -> Array (Int, Int) Char -> (Int, Int) -> [(Int, Int)] -> Int
+solve hdn m idx0 = foldl go 0
   where
-  m' = m Array.// [(idx, '#')]
-  w' = walk heading m' idx0
+  go :: Int -> (Int, Int) -> Int
+  go n idx = if repitition w' then succ n else n
+    where
+    m' = m Array.// [(idx, '#')]
+    w' = walk hdn m' idx0
 
 areaOfWalk :: Ord a => Walk a -> Int
 areaOfWalk w = Set.size $ Set.map snd $ collect w
@@ -59,51 +52,15 @@ repitition = go mempty
       | x `Set.member` acc -> True
       | otherwise -> go (Set.insert x acc) xs
 
--- | Returns the set of all position-direction pairs that have been
--- visited in this run.
-visitedPositions :: Ix ix => Heading ix -> Array ix Char -> ix -> (Set.Set (Direction ix, ix))
-visitedPositions heading m idx = fst $ head $ dropWhile (uncurry (/=)) $ zip coverages (tail coverages)
-  where
-  coverages = scanl (flip Set.insert) mempty $ walk heading m idx
-
 type Walk ix = [(Direction ix, ix)]
 
 walk :: Ix ix => Heading ix -> Array ix Char -> ix -> Walk ix
-walk heading m idx = iterateMaybe (step heading m) (North, idx)
+walk hdn m idx = iterateMaybe (step hdn m) (North, idx)
 
 iterateMaybe :: (a -> Maybe a) -> a -> [a]
 iterateMaybe f a = a : case f a of
   Nothing -> []
   Just b -> iterateMaybe f b
-
--- hasLoop :: forall ix . Ix ix => Heading ix -> Array ix Char -> ix -> Bool
--- hasLoop heading m ix = go mempty (uniquePrefix $ walk heading m ix)
---   where
---   go :: Set.Set (Direction ix, ix) -> Walk ix -> Bool
---   go _ [] = False
---   go s (x:xs)
---     | Set.member x s = True
---     | otherwise    = go (Set.insert x s) xs
-
--- uniquePrefix :: Eq a => [a] -> [a]
--- uniquePrefix [] = []
--- uniquePrefix xs = fmap snd $ takeWhile (uncurry (/=)) $ zip xs (tail xs)
-
-update :: Array (Int, Int) Char -> [(Int, Int)] -> [Array (Int, Int) Char]
-update m (ix:ixs) = m' : update m ixs
-  where
-  m' = m Array.// [(ix, 'X')]
-
-showMatrix :: (a -> String) -> Array (Int, Int) a -> String
-showMatrix shw a = unlines $ fmap (join . fmap shw) $ chunksOf (succ n) $ Array.elems a
-  where
-  (_, (n, _)) = Array.bounds a
-
-chunksOf :: Int -> [a] -> [[a]]
-chunksOf _ [] = []
-chunksOf n xs = a : chunksOf n b
-  where
-  (a, b) = splitAt n xs
 
 data Direction a = North | South | East | West
 
@@ -126,19 +83,21 @@ step
   -> Array ix Char
   -> (Direction ix, ix)
   -> Maybe (Direction ix, ix)
-step heading m (d, ix) = f <$> check heading m (d, ix)
+step hdn m (d, ix) = f <$> check hdn m (d, ix)
   where
-  f d' = (d', heading d' ix)
+  f d' = (d', hdn d' ix)
 
 check
-  :: Ix ix
+  :: forall ix
+  . Ix ix
   => Heading ix
   -> Array ix Char
   -> (Direction ix, ix)
   -> Maybe (Direction ix)
-check heading m = go 0
+check hdn m = go 0
   where
-  go n (d, ix) = case m !? heading d ix of
+  go :: Int -> (Direction ix, ix) -> Maybe (Direction ix)
+  go n (d, ix) = case m !? hdn d ix of
     _ | n == 4 -> Nothing
     Nothing -> Nothing
     Just '#' -> go (succ n) (turn d, ix)
