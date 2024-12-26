79556678ebf929b4207251fde70f3b6e489fa170
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-12-26 13:28:26 +0100

    2024-12-19 pt1

---
+++ 2024/19/Main.hs
@@ -0,0 +1,65 @@
+{-# language GHC2021, PartialTypeSignatures, LambdaCase #-}
+-- {-# options_ghc -Wall #-}
+module Main (main) where
+
+import Text.Parsec hiding (many, optional)
+import Data.Functor.Identity
+import Control.Exception hiding (try)
+import Data.Char
+import Control.Applicative
+import System.Environment
+import Text.Read
+import qualified Data.Map as Map
+import Data.Foldable
+import Data.Array (Array, Ix)
+import qualified Data.Array as Array
+import Control.Monad
+import qualified Data.List as List
+
+main :: IO ()
+main = do
+  inp <- getContents
+  case runParser parser () mempty inp of
+    Left e -> throwIO e
+    Right (candidates, b) -> do
+      let xs = filter (solve candidates) b
+      traverse_ putStrLn xs
+      print $ length xs
+
+solve :: [[Char]] -> [Char] -> Bool
+solve candidates xs = last $ Array.elems w
+  where
+  w = Array.listArray bounds (f <$> Array.range bounds)
+  u :: Array Int Char
+  u = Array.listArray bounds xs
+  bounds = (0, length xs)
+  f :: Int -> Bool
+  f ix
+    | ix <= 0 = True
+    | otherwise = not $ null $ do
+        candidate <- candidates
+        let jx = (ix - length (candidate))
+        guard $ jx >= 0
+        let x = slice u jx ix
+        guard $ x == candidate && w Array.! jx
+
+-- 398 is too high
+slice :: Enum ix => Ix ix => Array ix a -> ix -> ix -> [a]
+slice w a b = (w Array.!) <$> Array.range (a, pred b)
+
+split :: [Char] -> [Char] -> [[Char]]
+split x y
+  | x `List.isPrefixOf` y = pure $ drop (length x) y
+  | otherwise = []
+
+parser :: ParsecT String () Identity ([[Char]], [[Char]])
+parser = do
+  h <- header
+  newline
+  newline
+  (h,) <$> many (many (satisfy isAlpha) <* newline)
+
+header :: ParsecT String u Identity [[Char]]
+header
+  = many (satisfy isAlpha)
+  `sepBy` some (satisfy ((||) <$> (`elem` " ") <*> isPunctuation))
